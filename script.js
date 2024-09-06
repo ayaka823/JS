1a4d1479ce2b0b0226740cc0011a4b0dca48bd56
ayaka823 <ayaperidot23@gmail.com>
2024-09-06 21:03:51 +0900

    Create script.js
    コンソールでイベントの確認

---
+++ script.js
@@ -0,0 +1,6 @@
+window.alert("オブジェクトの取得に成功")
+const element = document.getElementById("list1");
+console.log(element);
+
+    let lists = document.getElementsByClassName("pull-down-list")
+        console.log(lists)
