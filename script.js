98e03287ad2adefbdb29060d496c17f1bdf78087
ayaka823 <ayaperidot23@gmail.com>
2024-09-11 21:32:46 +0900

    Update script.js
    イベント発火基礎編

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ pullDownButton.onmouseout = function() {
 pullDownButton.onclick = function() {
     console.log("プッシュ");
 };
+pullDownButton.addEventListener('mouseover', function(){
+    pullDownButton.setAttribute("style", "background-color:blue;")
+})
+pullDownButton.addEventListener('mouseout', function(){
+    pullDownButton.setAttribute("style", "background-color:red;")
+})
+pullDownButton.addEventListener('click', function(){
+    pullDownButton.setAttribute("style", "background-color:green;")
+})
