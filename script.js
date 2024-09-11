fd47869ace28a3a4f60b7e07ca17d0a464886f6a
ayaka823 <ayaperidot23@gmail.com>
2024-09-11 21:14:26 +0900

    Update script.js
    イベント発火基礎編

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ window.alert("オブジェクトの取得に成功")
 const element = document.getElementById("list1");
 console.log(element);
 
-    let lists = document.getElementsByClassName("pull-down-list")
-        console.log(lists)
+const pullDownButton = document.getElementById("lists");
+pullDownButton.onmouseover = function() {
+    console.log("オン");
+};
+pullDownButton.onmouseout = function() {
+    console.log("アウト");
+};
+pullDownButton.onclick = function() {
+    console.log("プッシュ");
+};
